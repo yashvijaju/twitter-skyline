e7d7a60dc22577e7bc2dbc821563fe4b22290a2f
sgkolli535 <sgkolli@usc.edu>
2023-02-05 02:15:53 -0800

    ihuih

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,10 +1,32 @@
+"use client"
 import * as React from 'react';
 import Router, { withRouter } from 'next/router'
 import './modal.css';
+import axios from 'axios';
 
+// export async function getSearchTweets(trend) {
+//   // Fetch data from external API
+//   const url = `https://api.twitter.com/1.1/search/tweets.json`;
+//   const res = await axios.get(url, {
+//     headers: {
+//       Authorization: `Bearer ${process.env.AUTH_BEARER}`
+//     },
+//     params: {
+//       'q': trend
+//     }
+//   });
+
+//   console.log('hello', res);
+
+//   // Pass data to the page via props
+//   return {
+//     props: {
+//       trends: res.data[0].statuses[0].urls[0].url,
+//     }}
+// }
 
 export default function Modal({props}) {
-    const {name, url} = props;
+    const {name, url, tweetUrl} = props;
   return (
     <>
         <div className='modal' />
@@ -12,6 +34,7 @@ export default function Modal({props}) {
             <div className='content'>
                 {name}
                 {url}
+                {tweetUrl}
             </div>
         </div>  
     </>
