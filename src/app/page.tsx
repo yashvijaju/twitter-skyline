65b3c392f596935ba78c718c8fe2abf7b5bcf005
sgkolli535 <sgkolli@usc.edu>
2023-02-04 19:12:46 -0800

    home page background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,98 @@
 "use client"
-import Image from 'next/image';
-import { Inter } from '@next/font/google';
-import styles from './page.module.css';
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import Link from '@mui/material/Link';
+import { createTheme } from '@mui/material/styles';
+import Script from 'next/script';
+import GLOBE from "./vanta.globe.min.js";
+import { useRef, useState, useEffect } from "react";
+import * as THREE from 'three';
+
+
+let theme = createTheme({
+  palette: {
+    primary: {
+      main: '#0x3fa4ff',
+    },
+    secondary: {
+      main: '#0xffffff',
+    },
+  },
+});
+
+theme = createTheme(theme, {
+  palette: {
+    info: {
+      main: theme.palette.secondary.main,
+    },
+  },
+});
 
 
 export default function Home() {
+
+  // const [vantaEffect, setVantaEffect] = useState(0);
+  // const vantaRef = useRef(null);
+
+  // useEffect(() => {
+  //   if (!vantaEffect) {
+  //     setVantaEffect(
+  //       GLOBE({
+  //         el: vantaRef.current,
+  //         THREE: THREE,
+  //         mouseControls: true,
+  //         touchControls: true,
+  //         gyroControls: false,
+  //         minHeight: 200.0,
+  //         minWidth: 200.0,
+  //         scale: 1.0,
+  //         scaleMobile: 1.0,
+  //         color: 0xfff33f
+  //       })
+  //     );
+  //   }
+  //   return () => {
+  //     if (vantaEffect) vantaEffect.destroy();
+  //   };
+  // }, [vantaEffect]);
+
   return (
-    <main className={styles.main}>
-    </main>
-  )
+    <Container maxWidth="lg">
+      {/* <div ref={vantaRef} style={{height: "100%", width: "100%", borderWidth: "2px", borderColor:"red"}}/> */}
+      <Box
+        sx={{
+          my: 4,
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Typography variant="h4" component="h1" gutterBottom>
+          Your Country's Tweets in 3D
+        </Typography>
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Age</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            
+            label="Age"
+            
+          >
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
+    </Container>
+  );
 }
